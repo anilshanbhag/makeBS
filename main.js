c98c1d5aa0a3ee8e2f9a4f91c037154a550bf37e
ashwin <ashwin@ashwin-SVE15127CNB.(none)>
2014-02-02 04:30:38 +0530

    sj:w

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
 var page = require('webpage').create(),
     system = require('system'),
     url;
-
+page.onConsoleMessage = function(msg) {
+	  console.log( msg);
+};
 phantom.injectJs('./button.js');
+phantom.injectJs('./navbar.js');
 
-var handlers = [ Button ];
+var handlers = [ Button , Navbar ];
 
 if (system.args.length === 1) {
     console.log('Usage: main.js <some URL>');
     phantom.exit();
 } else {
     url = system.args[1];
+	page.viewportSize = { width: 1280 , height: 600 };
     page.open(url, function (status) {
+		page.injectJs('./jquery.min.js');
         for(var i = 0; i < handlers.length; i++){
             var ret = page.evaluate(handlers[i]);
             console.log(ret);
         }
 
+		page.render('test.jpg');
         phantom.exit();
     });
 }
